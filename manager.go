88c27d91a676b1642f289529453fd6b276bbfa36
shyam sunder singh <shyamsunders507@gmail.com>
2024-08-29 23:42:10 +0530

    made manager to handle conn upgrade

---
+++ manager.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	websocketUpgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+type Manager struct {
+}
+
+func NewManager() *Manager {
+	return &Manager{}
+}
+
+func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+	log.Println(("new connection"))
+
+	// upgrade regular http connection into websocket
+	conn, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	conn.Close()
+}
